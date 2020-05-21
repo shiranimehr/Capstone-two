8727b7f890b7bf8fbac5802b8e3d69fd1e7ed651
Hooman Shirani-Mehr <hoomans2@illinois.edu>
2020-05-20 20:57:43 -0700

    First version.

---
+++ insincere_questions_classification/src/data/preprocessing.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import csv
+
+
+class DataProcessor:
+    """
+    Class for reading, processing, and writing data from the UCI
+    `Condition monitoring of hydraulic systems` dataset.
+    """
+    def __init__(self):
+        pass
+
+    def read_data(self, raw_data_path):
+        """Read raw data into DataProcessor."""
+        #do reading things
+
+    def process_data(self, stable=True):
+        """Process raw data into useful files for model."""
+        #do processing things
+
+    def write_data(self, processed_data_path):
+        """Write processed data to directory."""
+        #do writing things
